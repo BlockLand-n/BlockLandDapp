992622aa755f6ca169191b1d39f5e49d8828838f
nishant <nishantpacharne16@gmail.com>
2024-07-14 01:52:41 +0530

    added images preview

---
+++ src/app/dashboard/crop-commander/page.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { CiSquarePlus } from "react-icons/ci";
+
+const page = () => {
+  return (
+    <>
+      <div className="dark:bg-[#191C24] w-full min-h-screen">
+        <section className="min-h-screen pt-6">
+          <div className="w-full max-w-screen-xl px-4 mx-auto lg:px-12">
+            <div className="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+              <div className="flex-row items-center justify-between p-4 space-y-3 sm:flex sm:space-y-0 sm:space-x-4">
+                <div>
+                  <h5 className="mr-3 font-semibold dark:text-white">
+                    Track Your Farms
+                  </h5>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Manage all your existing farms or add a new one
+                  </p>
+                </div>
+                <button
+                  type="button"
+                  className="flex gap-2 items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 border border-[#4af058]"
+                >
+                  Invest in more
+                  <span className="text-2xl">
+                    <CiSquarePlus />
+                  </span>
+                </button>
+              </div>
+            </div>
+
+            <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden mt-6">
+
+            <div className="overflow-x-auto">
+                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" className="px-4 py-3">Farm</th>
+                            <th scope="col" className="px-4 py-3">Crop</th>
+                            <th scope="col" className="px-4 py-3">Invested</th>
+                            <th scope="col" className="px-4 py-3">P&L</th>
+                            <th scope="col" className="px-4 py-3">Status</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="border-b dark:border-gray-700">
+                            <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple iMac 27&#34;</th>
+                            <td className="px-4 py-3">PC</td>
+                            <td className="px-4 py-3">Apple</td>
+                            <td className="px-4 py-3">300</td>
+                            <td className="px-4 py-3">$2999</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            {/* <nav className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4" aria-label="Table navigation">
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                    Showing
+                    <span className="font-semibold text-gray-900 dark:text-white">1-10</span>
+                    of
+                    <span className="font-semibold text-gray-900 dark:text-white">1000</span>
+                </span>
+                <ul className="inline-flex items-stretch -space-x-px">
+                    <li>
+                        <a href="#" className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span className="sr-only">Previous</span>
+                            <svg className="w-5 h-5" aria-hidden="true" fill="currentColor"  xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                    </li>
+                    <li>
+                        <a href="#" aria-current="page" className="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
+                    </li>
+                    <li>
+                        <a href="#" className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span className="sr-only">Next</span>
+                            <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </nav> */}
+        </div>
+          </div>
+
+
+
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default page;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { pinFileToIPFS, pinJSONToIPFS } from "../utils/pinata";
 import { DataState } from "../provider";
 
@@ -11,20 +11,24 @@ const Page = () => {
   const [location, setLocation] = useState("");
   const [plant, setPlant] = useState("");
   const [farmSize, setFarmSize] = useState("");
-  const [uploadedFile, setUploadedFile] = useState(null);
-
+  const [capitalRequired, setCapital] = useState("");
+  const [profits, setProfits] = useState("");
+  const [expectedDate, setExpectedDate] = useState("");
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [showUploadField, setShowUploadField] = useState(true);
 
   const uploadMetadata = async () => {
-    if (!name || !description || !uploadedFile || !location || !farmSize) {
+    if (!name || !description || !uploadedFiles.length || !location || !farmSize) {
       alert("All fields are required");
       return;
     }
-
+  
     try {
-      // Upload the file to Pinata
-      const fileHash = await pinFileToIPFS(uploadedFile);
-      // console.log("File Hash:", fileHash);
-
+      // Upload each file to Pinata and get their hashes
+      const fileHashes = await Promise.all(uploadedFiles.map(async (file) => {
+        return await pinFileToIPFS(file);
+      }));
+  
       // Create metadata JSON
       const metadata = {
         name,
@@ -32,27 +36,47 @@ const Page = () => {
         location,
         plant,
         farmSize,
-        image: `https://gateway.pinata.cloud/ipfs/${fileHash}`,
+        images: fileHashes.map(hash => `https://gateway.pinata.cloud/ipfs/${hash}`), // Store each image URL in an array
       };
-
+  
       // Upload metadata to Pinata
       const metadataCID = await pinJSONToIPFS(metadata);
       console.log("Metadata CID:", metadataCID);
-
-
     } catch (error) {
       console.error("Error uploading metadata:", error);
       alert("There was an error uploading the metadata. Please try again.");
     }
   };
+  
 
-  const handleFileChange = (e) => {
-    setUploadedFile(e.target.files[0]);
+  useEffect(() => {
+    console.log(expectedDate);
+  }, [expectedDate])
+
+  const handleFileChange = (event) => {
+    const files = Array.from(event.target.files);
+    const imageUrls = files.map((file) => URL.createObjectURL(file));
+    console.log(imageUrls);
+    setUploadedFiles(files);
+    setShowUploadField(false);
+  };
+
+  const handleRemoveImages = () => {
+    setUploadedFiles([]);
+    setShowUploadField(true);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     await uploadMetadata();
+  };
+
+  const handleDate = async (e) => {
+  
+    // Convert expectedDate to Unix timestamp
+    // const expectedDateTimestamp = new Date(expectedDate).getTime() / 1000;
+  
+    setExpectedDate(e.target.value);
   };
 
   return (
@@ -121,8 +145,6 @@ const Page = () => {
                     />
                   </div>
 
-
-
                   <div className="sm:col-span-2">
                     <label
                       htmlFor="farmSize"
@@ -137,6 +159,56 @@ const Page = () => {
                       placeholder="Ex. 50 Acre"
                       value={farmSize}
                       onChange={(e) => setFarmSize(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="">
+                    <label
+                      htmlFor="capitalRequired"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Capital To Be Raised (in USDC)
+                    </label>
+                    <input
+                      type="number"
+                      id="capitalRequired"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Ex. 10000"
+                      value={capitalRequired}
+                      onChange={(e) => setCapital(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="">
+                    <label
+                      htmlFor="profits"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Expected Profits
+                    </label>
+                    <input
+                      type="number"
+                      id="profits"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      placeholder="Ex. 15000"
+                      value={profits}
+                      onChange={(e) => setProfits(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="sm:col-span-2">
+                    <label
+                      htmlFor="expectedDate"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Expected Harvest and Profit Distribution Date
+                    </label>
+                    <input
+                      type="date"
+                      id="expectedDate"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      value={expectedDate}
+                      onChange={(e) => handleDate(e.target.value)}
                     />
                   </div>
 
@@ -156,51 +228,78 @@ const Page = () => {
                     ></textarea>
                   </div>
 
-                  <div className="sm:col-span-2">
-                    <label
-                      htmlFor="upload"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Upload Images
-                    </label>
-                    <div className="flex items-center justify-center w-full">
-                      <label
-                        htmlFor="upload"
-                        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-                      >
-                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                          <svg
-                            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 20 16"
-                          >
-                            <path
-                              stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                            />
-                          </svg>
-                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                            <span className="font-semibold">Click to upload</span>{" "}
-                            or drag and drop
-                          </p>
-                          <p className="text-xs text-gray-500 dark:text-gray-400">
-                            SVG, PNG, JPG or GIF (MAX. 800x400px)
-                          </p>
-                        </div>
-                        <input
-                          id="upload"
-                          type="file"
-                          className="hidden"
-                          onChange={handleFileChange}
-                        />
-                      </label>
-                    </div>
-                  </div>
+                  {showUploadField ? (
+  <div className="sm:col-span-2">
+    <label
+      htmlFor="upload"
+      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    >
+      Upload Images
+    </label>
+    <div className="flex items-center justify-center w-full">
+      <label
+        htmlFor="upload"
+        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+      >
+        <div className="flex flex-col items-center justify-center pt-5 pb-6">
+          <svg
+            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 16"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            />
+          </svg>
+          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+            <span className="font-semibold">Click to upload</span>{" "}
+            or drag and drop
+          </p>
+          <p className="text-xs text-gray-500 dark:text-gray-400">
+            SVG, PNG, JPG or GIF (MAX. 800x400px)
+          </p>
+        </div>
+        <input
+          id="upload"
+          type="file"
+          className="hidden"
+          onChange={handleFileChange}
+          multiple
+        />
+      </label>
+    </div>
+  </div>
+) : (
+  <div className="sm:col-span-2">
+    <div className="flex justify-end mb-2">
+      <button
+        type="button"
+        className="text-red-500"
+        onClick={handleRemoveImages}
+      >
+        Remove Images
+      </button>
+    </div>
+    <div className="mt-4 grid grid-cols-2 gap-4">
+      {uploadedFiles.map((file, index) => (
+        <div key={index} className="relative">
+          <img
+            src={URL.createObjectURL(file)}
+            alt={`upload-${index}`}
+            className="w-full h-auto object-cover rounded-lg"
+          />
+        </div>
+      ))}
+    </div>
+  </div>
+)}
+
                 </div>
 
                 <button
