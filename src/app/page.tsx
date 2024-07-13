5b7142a0ce7f6e7e2256beed307beee4fa8ecf5c
nishant <nishantpacharne16@gmail.com>
2024-07-14 02:07:31 +0530

    connected pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { FaArrowRightLong } from "react-icons/fa6";
 import { DataState } from "./provider";
 import { useEffect, useState } from "react";
 import { checkConnection, retrievePublicKey } from "./stellar/freighter";
+import Link from "next/link";
+
 
 export default function Home() {
   const { address, setAddress } = DataState();
@@ -45,13 +47,10 @@ export default function Home() {
                 <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a> */}
 
-            <a
-              href="#"
-              className="inline-flex gap-2 items-center justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-[#4af058] dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            >
-              go to app
-              <FaArrowRightLong />
-            </a>
+            <Link className="inline-flex gap-2 items-center justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-[#4af058] dark:hover:bg-gray-700 dark:focus:ring-gray-800" href={"/onboarding"}> go to app
+              <FaArrowRightLong /></Link>
+
+
           </div>
         </div>
       </section>
